3e64e92496a4d88dceecce71a2ade09b31064245
Simon Chen <ZimChen@bayviewglen.ca>
2024-09-16 08:46:35 -0400

    Day 1 examples

---
+++ Unit One - introduction to Java/src/day1/ExampleTwo.java
@@ -0,0 +1,20 @@
+package day1;                      // semi colon is the end of the statement
+
+public class ExampleTwo {         // {} are used to indicates that sth is embedded inside sth else.
+
+    public static void main(String[] args) {
+       int numStudents = 14;  //variables use camelCase
+
+       double volume = 123.7; //double holds floating point decimal numbers
+
+       boolean largeCLass = numStudents > 20;  // boolean allows the comparison of values/variables
+
+       System.out.println(numStudents);
+       System.out.println(volume);
+       System.out.println(largeCLass);
+
+        int a = 7, b=6;  //you can identify 2 variables if they are the same type
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
